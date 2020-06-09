689785af1b3b1c3bb1ff6f1297dfbbebc5f41bf5
cyones <cristianyones@argeniss.com>
2020-06-09 19:21:44 -0300

    Added comments

--- iot_api/user_api/Inventory.py
+++ iot_api/user_api/Inventory.py
@@ -19,6 +19,17 @@ from collections import defaultdict
 
 
 class InventoryListAPI(Resource):
+    """ Endpoint to list assets (devices + gateways)
+    Request arguments (all optional):
+    - page: for paginate.
+    - size: for paginate.
+    - vendor: for filtering, lists only assets with this vendor.
+    - gateway_id: for filtering, list only this gateway and the devices connected to it.
+    - data_collector_id: for filtering, list only the assest related to this data collector.
+    - device_type: for filtering, list only this type of devices ("device" or "gateway").
+    Returns:
+    JSON with list of assets (see code for more details about the sub fields).
+    """
     @jwt_required
     def get(self):
         try:
@@ -36,6 +47,7 @@ class InventoryListAPI(Resource):
             data_collector_id = request.args.get('data_collector_id', default=None, type=int)
             device_type = request.args.get('device_type', default=None, type=str)
 
+            # Build two queries, one for devices and one for gateways
             s1 = select([
                 Device.dev_eui.label('id'),
                 expression.literal_column('\'Device\'').label('type'),
@@ -57,6 +69,7 @@ class InventoryListAPI(Resource):
                     where(Gateway.organization_id == organization_id).\
                     where(Gateway.data_collector_id == DataCollector.id)
 
+            # If filter parameters were given, add the respective where clauses to the queries
             if vendor:
                 s1 = s1.where(Device.vendor == vendor)
                 s2 = s2.where(Gateway.vendor == vendor)
@@ -67,6 +80,7 @@ class InventoryListAPI(Resource):
                 s1 = s1.where(DataCollector.id == data_collector_id)
                 s2 = s2.where(DataCollector.id == data_collector_id)
 
+            # Filter by device type if the parameter was given, else, make a union with queries.
             query = s1.union(s2)
             if device_type:
                 if device_type == "device":
@@ -76,6 +90,7 @@ class InventoryListAPI(Resource):
                 else:
                     raise Exception("Invalid device type parameter")
 
+            # Execute the queries and join the results
             query = query.order_by(text('type desc'))
             query = query.alias('device_gateway')
             results = db.session.query(query).paginate(page=page, per_page=size)
@@ -97,6 +112,17 @@ class InventoryListAPI(Resource):
 
 
 class InventoryCountAPI(Resource):
+    """ Endpoint to count assets (devices + gateways). It simply parses the arguments and call
+    the functions to count the assets.
+    Request arguments (all optional):
+    - vendor: for filtering, counts only assets with this vendor.
+    - gateway_id: for filtering, only counts this gateway and the devices connected to it.
+    - data_collector_id: for filtering, only counts the assest related to this data collector.
+    - device_type: for filtering, only counts this type of devices ("device" or "gateway").
+    Returns:
+    - A JSON with 4 fields, the names are self-explanatory: n_assets_per_vendor, n_assets_per_gateway, 
+        n_assets_per_datacollector, n_assets_per_tags
+    """
     @jwt_required
     def get(self):
         try:
@@ -149,6 +175,8 @@ class InventoryCountAPI(Resource):
 
 
 def n_asset_per_vendor(organization_id, vendor=None, gateway_id=None, data_collector_id=None, device_type=None):
+    """ Count the number of assets per vendor. All the parameters are for filtering """
+    # Build two queries, one for devices and one for gateways
     s1 = db.session.query(Device.vendor, func.count(Device.id)).\
         join(GatewayToDevice).\
         join(DataCollectorToDevice).\
@@ -159,6 +187,7 @@ def n_asset_per_vendor(organization_id, vendor=None, gateway_id=None, data_colle
         group_by(Gateway.vendor).\
         filter(Gateway.organization_id==organization_id)
 
+    # If the filtering arguments are given, add the respective where clauses to the queries
     if vendor:
         s1 = s1.filter(Device.vendor==vendor)
         s2 = s2.filter(Gateway.vendor==vendor)
@@ -169,16 +198,14 @@ def n_asset_per_vendor(organization_id, vendor=None, gateway_id=None, data_colle
         s1 = s1.filter(DataCollector.id == data_collector_id)
         s2 = s2.filter(Gateway.data_collector_id == data_collector_id)
 
-    dev_per_vendor = s1.all()
-    gw_per_vendor = s2.all()
-
-    all_queries = []
+    # Execute the queries, filtering by asset type
+    all_queries = None
     if device_type == "device":
-        all_queries = dev_per_vendor
+        all_queries = s1.all()
     elif device_type == "gateway":
-        all_queries = gw_per_vendor
+        all_queries = s2.all()
     elif device_type is None:
-        all_queries = dev_per_vendor + gw_per_vendor
+        all_queries = s1.all() + s2.all()
 
     counts = defaultdict(lambda: 0)
     for e in all_queries:
@@ -187,12 +214,15 @@ def n_asset_per_vendor(organization_id, vendor=None, gateway_id=None, data_colle
 
 
 def n_asset_per_gateway(organization_id, vendor=None, gateway_id=None, data_collector_id=None, device_type=None):
+    """ Count the number of assets per gateway. All the parameters are for filtering """
+    # Query to count the number of devices per gateway
     query = db.session.query(Gateway.gw_hex_id, func.count(Gateway.gw_hex_id)).\
         join(GatewayToDevice).\
         join(Device).\
         group_by(Gateway.gw_hex_id).\
         filter(Gateway.organization_id==organization_id)
     
+    # If the arguments are given, filter adding the respective where clause
     if vendor:
         query=query.filter(or_(Device.vendor==vendor, Gateway.vendor==vendor))
     if gateway_id:
@@ -200,17 +230,20 @@ def n_asset_per_gateway(organization_id, vendor=None, gateway_id=None, data_coll
     if data_collector_id:
         query=query.filter(Gateway.data_collector_id==data_collector_id)
     
-    counts = defaultdict(lambda: 1) # One because the gateway counts as an asset
+    # Execute the query and build the response
+    counts = defaultdict(lambda: 1) # Starts with one because the gateway counts as an asset
     for e in query.all():
-        counts[e[0]] += e[1]
+        counts[e[0]] += e[1] # Sum the number of devices per gateway + 1 (the gateway)
         if device_type=="device":
-            counts[e[0]]-= 1
+            counts[e[0]]-= 1 # Subtract one because the gateway has not to be taken into account
         elif device_type=="gateway":
-            counts[e[0]] = 1
+            counts[e[0]] = 1 # If only want to count gateways, the response is always one per gateway_id
     return dict(counts)
 
 
 def n_asset_per_datacollector(organization_id, vendor=None, gateway_id=None, data_collector_id=None, device_type=None):
+    """ Count the number of assets per data collector. All the parameters are for filtering """
+    # Base queries, one for devices and one for gateways
     s1 = db.session.query(DataCollector.name, func.count(DataCollector.id)).\
         join(DataCollectorToDevice).\
         join(Device).\
@@ -221,6 +254,7 @@ def n_asset_per_datacollector(organization_id, vendor=None, gateway_id=None, dat
         group_by(DataCollector.id).\
         filter(DataCollector.organization_id==organization_id)
 
+    # If some filtering parameters are giveng, add the respective where clauses to the queries
     if vendor:
         s1 = s1.filter(Device.vendor==vendor)
         s2 = s2.filter(Gateway.vendor==vendor)
@@ -231,9 +265,11 @@ def n_asset_per_datacollector(organization_id, vendor=None, gateway_id=None, dat
         s1 = s1.filter(DataCollector.id == data_collector_id)
         s2 = s2.filter(Gateway.data_collector_id == data_collector_id)
 
+    # Run the queries
     dev_per_dc = s1.all()
     gw_per_dc = s1.all()
 
+    # Filter the queries by asset type if the parameter is given
     all_queries = []
     if device_type == "device":
         all_queries = dev_per_dc
@@ -242,6 +278,7 @@ def n_asset_per_datacollector(organization_id, vendor=None, gateway_id=None, dat
     elif device_type is None:
         all_queries = dev_per_dc + gw_per_dc
 
+    # Join the results of the queries
     counts = defaultdict(lambda: 0)
     for e in dev_per_dc + gw_per_dc:
         counts[e[0]] += e[1]
@@ -249,6 +286,8 @@ def n_asset_per_datacollector(organization_id, vendor=None, gateway_id=None, dat
 
 
 def n_asset_per_tag(organization_id, vendor=None, gateway_id=None, data_collector_id=None, device_type=None):
+    """ Count the number of assets per tag. All the parameters are for filtering """
+    """ TODO: this function returns the total number of assets since we dont have tags for now """
     total_devs = db.session.query(func.count(Device.id)).\
         filter(Device.organization_id==organization_id).all()
     total_gws = db.session.query(func.count(Gateway.id)).\
