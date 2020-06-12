040aa38678a286a42b00cde14727f394af8c1050
cyones <cyones77@gmail.com>
2020-06-12 16:29:10 -0300

    Removed repeated code

--- iot_api/user_api/Utils.py
+++ iot_api/user_api/Utils.py
@@ -3,7 +3,6 @@ from iot_api.user_api.enums import RoleTypes
 
 def is_admin_user(user_id):
     """ verify if specified username belongs to user with role 'User_Admin' """
-
     role = UserRole.find_by_role_name(RoleTypes.User_Admin.value)
     if not role:
         return None
@@ -29,18 +28,6 @@ def is_regular_user(user_id):
         return False
 
 
-def is_admin_user(user_id):
-    """ verify if specified username belongs to user with role 'User_Admin'"""
-    role = UserRole.find_by_role_name(RoleTypes.User_Admin.value)
-    if not role:
-        return None
-    role_id = role.id
-    if not role_id:
-        return None
-    if UserToUserRole.find_by_user_id_and_user_role_id(user_id, role_id):
-        return True
-    else:
-        return False
 
 
 # verify if specified username is system
